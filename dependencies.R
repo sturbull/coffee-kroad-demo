d4d9c40d7b116056ae13991ea8d2cee2660a2e66
Steven Turnbull <stur600@gmail.com>
2025-06-06 16:16:25 +1200

    installed bslib

--- dependencies.R
+++ dependencies.R
@@ -1,6 +1,7 @@
 # This file allows packrat (used by rsconnect during deployment) to pick up dependencies.
 library(box)
 library(box.linters)
+library(bslib)
 library(leaflet)
 library(rhino)
 library(shiny.router)
